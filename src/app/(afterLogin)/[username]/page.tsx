2e804f54e1157d6d22a9384e76e6cbc943e56b3d
Seowoo Choi <264826@naver.com>
2025-01-05 22:23:57 +0900

    File: 라우팅 설정

---
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <button>Home</button>
+      <button>Home1</button>
+      <button>Home2</button>
+      <button>Home3</button>
+    </div>
+  );
+}
---
+++ src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default async function HomeLayout({ children }:{ children: React.ReactNode }) {
+  return (
+    <div>HomeLayout{ children }</div>
+  )
+}
